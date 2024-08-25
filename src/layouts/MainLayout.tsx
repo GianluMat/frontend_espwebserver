cf84b9ae5a0957d9b67534c7e01614904da35062
GianluMat <gianluca.mattiello89@gmail.com>
2024-08-25 02:01:57 +0200

    tolta topbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
-import { TopBar } from "../components/TopBar";
+// import { TopBar } from "../components/TopBar";
 
 export const MainLayout: React.FC = () => {
   return (
